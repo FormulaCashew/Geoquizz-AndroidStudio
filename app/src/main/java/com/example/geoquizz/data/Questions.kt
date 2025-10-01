13b578e351af814ca76855dfbb13ec0a750306f3
FormulaCashew <acerty19@gmail.com>
2025-10-01 11:45:27 -0600

    Assigned id and answers to questions

--- app/src/main/java/com/example/geoquizz/data/Questions.kt
+++ app/src/main/java/com/example/geoquizz/data/Questions.kt
@@ -1,6 +1,17 @@
 package com.example.geoquizz.data
+import com.example.geoquizz.R
 
 data class Questions(
-    val text: String,
-    val answer: Boolean
+    val id : Int,   // id of question
+    val textRes: Int, //text resource
+    val answer: Boolean // expected correct answer
+)
+
+// match question with correct answer and id
+val questionBank = listOf(
+    Questions(0, R.string.question_oceans, true),
+    Questions(1,R.string.question_mideast, false),
+    Questions(2,R.string.question_africa, false),
+    Questions(3,R.string.question_americas, true),
+    Questions(4,R.string.question_asia, true)
 )
