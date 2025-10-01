8f2b99d59741aaf027501832724bd9e7236b54ab
FormulaCashew <acerty19@gmail.com>
2025-10-01 08:45:45 -0600

    Added necessary stringsated the basic app layout

---
+++ app/src/main/java/com/example/geoquizz/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.geoquizz
+
+import android.R.color.black
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.geoquizz.ui.theme.GeoQuizzTheme
+import com.example.geoquizz.data.ContentViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeoQuizzTheme {
+                    Geoquizz()
+            }
+        }
+    }
+}
+
+@Composable
+fun Geoquizz(){
+    Surface(modifier = Modifier
+        .fillMaxSize(),
+        color = Color.Black
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("HI")
+            AnswerButtons()
+        }
+    }
+}
+
+@Composable
+fun AnswerButtons(){
+    Row {
+        Button(
+            modifier = Modifier,
+            onClick = {}
+        ) {
+            Text("False")
+        }
+        Button(
+            modifier = Modifier,
+            onClick = {}
+        ) {
+            Text("True")
+        }
+    }
+}
+
+@Composable
+fun IndexButtons(){
+    Row {
+        Button(
+            modifier = Modifier,
+            onClick = {increment_index()}
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.arrow_back_ios_24),
+                contentDescription = "Previous Question"
+            )
+        }
+        Button(
+            modifier = Modifier,
+            onClick = {}
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.arrow_forward_ios_24),
+                contentDescription = "Next Question"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GeoQuizzTheme {
+        Geoquizz()
+    }
+}
