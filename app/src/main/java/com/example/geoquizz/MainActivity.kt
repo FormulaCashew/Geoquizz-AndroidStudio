c3d3764a048f200c97f41572feb82c9a48051039
FormulaCashew <acerty19@gmail.com>
2025-10-01 13:02:12 -0600

    Updated to use methods and classes previously created

--- app/src/main/java/com/example/geoquizz/MainActivity.kt
+++ app/src/main/java/com/example/geoquizz/MainActivity.kt
@@ -13,15 +13,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.geoquizz.ui.theme.GeoQuizzTheme
 import com.example.geoquizz.data.ContentViewModel
 
@@ -38,7 +41,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Geoquizz(){
+fun Geoquizz(viewModel: ContentViewModel = viewModel()){
+    val uiState by viewModel.uiState.collectAsState()
+
     Surface(modifier = Modifier
         .fillMaxSize(),
         color = Color.Black
@@ -47,7 +52,12 @@ fun Geoquizz(){
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("HI")
+            //questions
+            Text(
+                text = uiState.currQuesText,
+                color = Color.White,
+                fontSize = 24.sp
+            )
             AnswerButtons()
         }
     }
