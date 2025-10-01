897c6b6919f560c4a884c2fa0b328603f7151458
FormulaCashew <acerty19@gmail.com>
2025-10-01 14:44:32 -0600

    Added index change test

--- app/src/test/java/com/example/geoquizz/ExampleUnitTest.kt
+++ app/src/test/java/com/example/geoquizz/ExampleUnitTest.kt
@@ -1,17 +1,41 @@
-package com.example.geoquizz
+package com.example.geoquizz.data
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Before
+import android.app.Application
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+class TestApplication : Application() {
+}
+
+class ContentViewModelUnitTest {
+
+    private val testApplication = TestApplication()
+
+    private lateinit var viewModel: ContentViewModel
+    private val totalQuestions = questionBank.size // Should be 5
+
+    @Before
+    fun setup() {
+        viewModel = ContentViewModel(testApplication)
     }
+
+    @Test
+    fun viewModel_initialization_setsCorrectState() {
+        val initialIndex = viewModel.uiState.value.currQuesIndex
+
+        viewModel.nextQuest()
+
+        val newIndex = viewModel.uiState.value.currQuesIndex
+
+        //check for changed index
+        assertEquals(initialIndex + 1, newIndex)
+        assertEquals(1, newIndex)
+    }
+
 }
