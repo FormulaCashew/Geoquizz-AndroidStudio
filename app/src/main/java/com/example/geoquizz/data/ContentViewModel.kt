bc2b7b39a565937b8601f378a25f68b84d9670f4
FormulaCashew <acerty19@gmail.com>
2025-10-01 11:40:09 -0600

    Added variables to save IDs and UI states

--- app/src/main/java/com/example/geoquizz/data/ContentViewModel.kt
+++ app/src/main/java/com/example/geoquizz/data/ContentViewModel.kt
@@ -11,8 +11,11 @@ import kotlinx.coroutines.flow.update
 
 class ContentViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val questions: List<Questions>
+    private val questions: List<Questions> //list of questions
+    private val CorrectAnsID = mutableSetOf<Int>() //which ones are correct
 
+    private val _uiState : MutableStateFlow<QuizStateUI> //save state
+    val uiState : StateFlow<QuizStateUI> // keep track of ui
 
     init {
         questions = loadQuestions()
