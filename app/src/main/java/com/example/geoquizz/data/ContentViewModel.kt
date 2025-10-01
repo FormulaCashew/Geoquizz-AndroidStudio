64f3ddff9c65940e81a200ddfe15d70315fd7349
FormulaCashew <acerty19@gmail.com>
2025-10-01 11:55:02 -0600

    Assigned initializations according to used classes

--- app/src/main/java/com/example/geoquizz/data/ContentViewModel.kt
+++ app/src/main/java/com/example/geoquizz/data/ContentViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.geoquizz.data
 
 import android.app.Application
-import androidx.databinding.tool.util.Resources
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,18 +10,25 @@ import kotlinx.coroutines.flow.update
 
 class ContentViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val questions: List<Questions> //list of questions
+    private val questions = questionBank //list of questions
     private val CorrectAnsID = mutableSetOf<Int>() //which ones are correct
 
     private val _uiState : MutableStateFlow<QuizStateUI> //save state
     val uiState : StateFlow<QuizStateUI> // keep track of ui
 
     init {
-        questions = loadQuestions()
+        val firstQuestTxt = getQuestText(questions.first().textRes)
+        _uiState = MutableStateFlow(
+            QuizStateUI(
+                firstQuestTxt,
+                questions.size
+            )
+        )
+        uiState = _uiState.asStateFlow()
     }
 
-    private fun loadQuestions(){
-        val stringArr = getApplication<Application>().Resources.getStringArray
+    private fun getQuestText(resId:Int): String{
+        return getApplication<Application>().getString(resId)
     }
 
 }
