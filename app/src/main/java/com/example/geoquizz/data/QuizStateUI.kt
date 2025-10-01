e7d1b9e107a56253ecc337f3f5ce07c07f9005c6
FormulaCashew <acerty19@gmail.com>
2025-10-01 08:56:09 -0600

    Created necessary parameters for data class

--- app/src/main/java/com/example/geoquizz/data/QuizStateUI.kt
+++ app/src/main/java/com/example/geoquizz/data/QuizStateUI.kt
@@ -1,4 +1,8 @@
 package com.example.geoquizz.data
 
-class QuizStateUI {
-}
+class QuizStateUI (
+    val currQuesText: String = "",
+    val currQuesIndex: Int = 0,
+    val score: Int = 0,
+    val totalQues: Int = 0
+)
