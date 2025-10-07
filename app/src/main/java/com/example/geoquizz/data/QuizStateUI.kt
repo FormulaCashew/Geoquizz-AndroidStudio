fadec36391062ddf1e9e6a63172981d378b753cb
FormulaCashew <acerty19@gmail.com>
2025-10-07 11:39:53 -0600

    Added new boolean field

--- app/src/main/java/com/example/geoquizz/data/QuizStateUI.kt
+++ app/src/main/java/com/example/geoquizz/data/QuizStateUI.kt
@@ -4,5 +4,6 @@ data class QuizUIState (
     val currQuesText: String = "",
     val currQuesIndex: Int = 0,
     val score: Int = 0,
-    val totalQues: Int = 0
+    val totalQues: Int = 0,
+    val lastAnswerCorrect: Boolean? = null // 👈 null = no answer yet
 )
