2f960eb5eec478522ccbc5576556f5f2b43b7a83
FormulaCashew <acerty19@gmail.com>
2025-10-07 12:52:12 -0600

    Added tests for wrapping

--- app/src/test/java/com/example/geoquizz/ExampleUnitTest.kt
+++ app/src/test/java/com/example/geoquizz/ExampleUnitTest.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 import org.junit.Assert.*
 import org.junit.Before
 import android.app.Application
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.runner.RunWith
 
@@ -13,67 +12,30 @@ import org.junit.runner.RunWith
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class TestApplication : Application() {
-}
 
-@RunWith(AndroidJUnit4::class)
-class ContentViewModelInstrumentedTest {
+class ContentViewModelTest  {
 
     private lateinit var viewModel: ContentViewModel
 
     @Before
     fun setup() {
-        val context = ApplicationProvider.getApplicationContext<Application>()
-        viewModel = ContentViewModel(context)
+        viewModel = ContentViewModel()
     }
 
     @Test
     fun nextQuestion_changesIndex() {
-        val initial = viewModel.uiState.value.currQuesIndex
+        // check if question index changes
+        val initialIndex = viewModel.uiState.value.currQuesIndex
         viewModel.nextQuest()
         val newIndex = viewModel.uiState.value.currQuesIndex
-        assertEquals((initial + 1) % questionBank.size, newIndex)
+        assertEquals((initialIndex + 1) % questionBank.size, newIndex)
     }
 
-//    @Test
-//    fun viewModel_initialization_setsCorrectState() {
-//        val initialIndex = viewModel.uiState.value.currQuesIndex
-//
-//        viewModel.nextQuest()
-//
-//        val newIndex = viewModel.uiState.value.currQuesIndex
-//
-//        //check for changed index
-//        assertEquals(initialIndex + 1, newIndex)
-//        assertEquals(1, newIndex)
-//    }
-//    @Test
-//    fun checkAns_stateIsPersistedAfterMove() {
-//
-//        viewModel.checkAns(true)
-//
-//        val finalState = viewModel.uiState.value
-//
-//        //Check if value is saved
-//        assertEquals(1, finalState.currQuesIndex)
-//        assertEquals(1, finalState.score)
-//    }
-//
-//    @Test
-//    fun checkAns_correctAnswerGivenTwice_scoreIncrementsOnlyOnce() {
-//        viewModel.checkAns(true)
-//        viewModel.nextQuest()
-//        viewModel.prevQuest()
-//        viewModel.checkAns(true)
-//
-//        //Check if correct ans is saved
-//        assertEquals(1, viewModel.uiState.value.score)
-//    }
-//    @Test
-//    fun checkAns_correctThenIncorrectAnswer_scoreIncrementsOnlyOnce() {
-//        viewModel.checkAns(true)
-//        viewModel.checkAns(true)
-//
-//        assertEquals(1, viewModel.uiState.value.score)
-//    }
+    @Test
+    fun prevQuestion_wrapsAroundCorrectly() {
+        // from 0 wrap to last
+        viewModel.prevQuest()
+        val newIndex = viewModel.uiState.value.currQuesIndex
+        assertEquals(questionBank.size - 1, newIndex)
+    }
 }
